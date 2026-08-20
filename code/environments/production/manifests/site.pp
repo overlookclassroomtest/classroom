1a7c921f451c5f78d41ce315100dcc4f82b4a2c8
overlookclassroomtest <tinfoil-axle.59@icloud.com>
2026-08-20 18:30:58 +0000

    where does this commit go?

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -3,4 +3,6 @@ node default {
     message => "Hello from ${trusted['certname']}, running \
 ${facts['os']['name']} ${facts['os']['release']['full']} on ${facts['os']['architecture']}.",
   }
+
+  notify { "what a bummer that this doesn't have syntax highlighting": }
 }
